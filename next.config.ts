52eda21e5273001b87b7c8abf1957fd01a5b114d
Astro <oceanrabbit0101@protonmail.com>
2026-03-29 16:16:58 +0200

    removed pug-loader

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack(config) {
+/*   webpack(config) {
     config.module.rules.push({
       test: /\.pug$/,
       use: require('pug-loader')
     });
     return config;
-  }
+  } */
 };
 
 export default nextConfig;
